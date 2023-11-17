ca5c4906c03d74ce0a5ceeebb93b8394514bb29b
adbarinov <115462546+adbarinov@users.noreply.github.com>
2023-11-17 08:36:53 +0300

    Add files via upload

---
+++ rtl/impl_top.sv
@@ -0,0 +1,87 @@
+
+// from https://github.com/ben-marshall/uart/
+// Module: impl_top
+// 
+// Notes:
+// - Top level module to be used in an implementation.
+// - To be used in conjunction with the constraints/defaults.xdc file.
+// - Ports can be (un)commented depending on whether they are being used.
+// - The constraints file contains a complete list of the available ports
+//   including the chipkit/Arduino pins.
+//
+
+module impl_top (
+input  logic        clk     , // Top level system clock input.
+input  logic        sw_0    , // Slide switches.
+input  logic        sw_1    , // Slide switches.
+input  logic        uart_rxd, // UART Recieve pin.
+output logic        uart_txd, // UART transmit pin.
+output logic [7:0]  led
+);
+
+// Clock frequency in hertz.
+parameter CLK_HZ = 50000000;
+parameter BIT_RATE =   9600;
+parameter PAYLOAD_BITS = 8;
+
+logic [PAYLOAD_BITS-1:0]  uart_rx_data;
+logic        uart_rx_valid;
+logic        uart_rx_break;
+
+logic        uart_tx_busy;
+logic [PAYLOAD_BITS-1:0]  uart_tx_data;
+logic        uart_tx_en;
+
+logic [PAYLOAD_BITS-1:0]  led_reg;
+assign      led = led_reg;
+
+// ------------------------------------------------------------------------- 
+
+assign uart_tx_data = uart_rx_data;
+assign uart_tx_en   = uart_rx_valid;
+
+always_ff @(posedge clk) begin
+    if(!sw_0) begin
+        led_reg <= 8'hF0;
+    end else if(uart_rx_valid) begin
+        led_reg <= uart_rx_data[7:0];
+    end
+end
+
+
+// ------------------------------------------------------------------------- 
+
+//
+// UART RX
+uart_rx #(
+.BIT_RATE(BIT_RATE),
+.PAYLOAD_BITS(PAYLOAD_BITS),
+.CLK_HZ  (CLK_HZ  )
+) i_uart_rx(
+.clk          (clk          ), // Top level system clock input.
+.resetn       (sw_0         ), // Asynchronous active low reset.
+.uart_rxd     (uart_rxd     ), // UART Recieve pin.
+.uart_rx_en   (1'b1         ), // Recieve enable
+.uart_rx_break(uart_rx_break), // Did we get a BREAK message?
+.uart_rx_valid(uart_rx_valid), // Valid data recieved and available.
+.uart_rx_data (uart_rx_data )  // The recieved data.
+);
+
+//
+// UART Transmitter module.
+//
+uart_tx #(
+.BIT_RATE(BIT_RATE),
+.PAYLOAD_BITS(PAYLOAD_BITS),
+.CLK_HZ  (CLK_HZ  )
+) i_uart_tx(
+.clk          (clk          ),
+.resetn       (sw_0         ),
+.uart_txd     (uart_txd     ),
+.uart_tx_en   (uart_tx_en   ),
+.uart_tx_busy (uart_tx_busy ),
+.uart_tx_data (uart_tx_data ) 
+);
+
+
+endmodule
