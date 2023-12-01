99427e266f5971594c09e8c63dd783748ce2df68
adbarinov <115462546+adbarinov@users.noreply.github.com>
2023-12-01 17:13:32 +0300

    Изменена логика работы recieved_data, комментарии переведены на русский язык
    Глобально - изменена логика работы регистра recieved_data, локально - комментарии переведены на  русский язык.

--- uart/rtl/uart_rx.sv
+++ uart/rtl/uart_rx.sv
@@ -1,81 +1,80 @@
-// from https://github.com/ben-marshall/uart/
-// Module: uart_rx 
+// из https://github.com/ben-marshall/uart/
+// Модуль: uart_rx 
 // 
-// Notes:
+// Примечание:
 // - UART reciever module.
 //
 
 module uart_rx (
-input  logic       clk          , // Top level system clock input.
-input  logic       resetn       , // Asynchronous active low reset.
-input  logic       uart_rxd     , // UART Recieve pin.
-input  logic       uart_rx_en   , // Recieve enable
-output logic       uart_rx_break, // Did we get a BREAK message?
-output logic       uart_rx_valid, // Valid data recieved and available.
-output logic [PAYLOAD_BITS-1:0] uart_rx_data   // The recieved data.
+input  logic       clk          , // Системный тактовый сигнал
+input  logic       resetn       , // Асинхронный глобальный сброс (активный уровень 0)
+input  logic       uart_rxd     , // Пин принимаемых UART значений
+input  logic       uart_rx_en   , // Разрешение приёма данных
+output logic       uart_rx_break, // Было ли получено сообщение прерывании приёма?
+output logic       uart_rx_valid, // Получены и доступны достоверные данные
+    output logic [PAYLOAD_BITS-1:0] uart_rx_data   // Принятые данные
 );
 
 // --------------------------------------------------------------------------- 
-// External parameters
+// Внешние параметры
 // --------------------------------------------------------------------------- 
 
-// Input bit rate of the UART line
+// Скорость передачи данных UART
 parameter BIT_RATE = 9600;
 localparam  BIT_P           = 1_000_000_000 * 1/BIT_RATE; // nanoseconds
 
-// Clock frequency in hertz
+// Частота тактового сигнала в Гц
 parameter CLK_HZ = 50_000_000;
 localparam  CLK_P           = 1_000_000_000 * 1/CLK_HZ; // period in nanoseconds
 
-// Number of data bits recieved per UART packet
+// Количество информационны бит-данных, принимаетмых UART
 parameter PAYLOAD_BITS    = 8;
 
-// Number of stop bits indicating the end of a packet
+// Количество стоп-бит, определяющих завершение принимаемой посылки
 parameter STOP_BITS       = 1;
 
 // -------------------------------------------------------------------------- 
-// Internal parameters
+// Внутренние параметры
 // ---------------------------------------------------------------------------
 
-// Number of clock cycles per uart bit
+// Количество циклов тактового сигнала на один принимаемый бит
 localparam       CYCLES_PER_BIT     = BIT_P / CLK_P;
 
-// Size of the registers which store sample counts and bit durations
+// Размер регистров, хранящих количество выборок и длительность битов
 localparam       COUNT_REG_LEN      = 1+$clog2(CYCLES_PER_BIT);
 
 // -------------------------------------------------------------------------- 
-// Internal registers
+// Внутренние регистры
 // ---------------------------------------------------------------------------
 
-// Internally latched value of the uart_rxd line for syncronization
-logic rxd_reg;
-logic rxd_reg_0;
+    // Регистр, через который осуществляется синхронизация принимаемых извне значений (два триггера)
+logic [1:0] rxd_reg;
 
-// Storage for the recieved serial data
+// Регистр, хранящий принятые последовательные данные
 logic [PAYLOAD_BITS-1:0] recieved_data;
 
-// Counter for the number of cycles over a packet bit
+// Счётчик количества циклов тактового сигнала в пакетном бите
 logic [COUNT_REG_LEN-1:0] cycle_counter;
 
-// Counter for the number of recieved bits of the packet
+// Счётчик количества принятый в пакете бит
 logic [$clog2(COUNT_REG_LEN)-1:0] bit_counter;
 
-// Sample of the UART input line whenever we are in the middle of a bit frame
+// Захват и хранение поступающих данных, в момент чтения середины фрейма входного бита
 logic bit_sample;
 
-// Current and next states of the internal FSM
+// Определение текущего и следующего состояний внутреннего конечного автомата
 typedef enum logic [1:0] {FSM_IDLE = 2'b00, FSM_START, FSM_RECV, FSM_STOP } state_t;
 state_t fsm_state, n_fsm_state;
 
 // --------------------------------------------------------------------------- 
-// Output assignment
+// Назначение выходных данных
 // ---------------------------------------------------------------------------
 
 assign uart_rx_break = (uart_rx_valid && ~|recieved_data);
 assign uart_rx_valid = ((fsm_state == FSM_STOP) && (n_fsm_state == FSM_IDLE));
 
 always_ff @(posedge clk) begin
-    if(!resetn) begin
+    if (!resetn) begin
         uart_rx_data  <= {PAYLOAD_BITS{1'b0}};
     end else if (fsm_state == FSM_STOP) begin
         uart_rx_data  <= recieved_data;
@@ -83,7 +82,7 @@ always_ff @(posedge clk) begin
 end
 
 // --------------------------------------------------------------------------- 
-// FSM next state selection
+// Выбор следющего состояния конечного автомата
 // ---------------------------------------------------------------------------
 
 logic next_bit;
@@ -94,9 +93,9 @@ assign next_bit = ((cycle_counter == CYCLES_PER_BIT) ||
                     (cycle_counter   == CYCLES_PER_BIT/2));
 assign payload_done = (bit_counter   == PAYLOAD_BITS)  ;
 
-// Handle picking the next state
+// Логика выбора следующего состояния
 always_comb begin : p_n_fsm_state
-    case(fsm_state)
+    case (fsm_state)
         FSM_IDLE : n_fsm_state = (rxd_reg      ? FSM_IDLE : FSM_START);
         FSM_START: n_fsm_state = (next_bit     ? FSM_RECV : FSM_START);
         FSM_RECV : n_fsm_state = (payload_done ? FSM_STOP : FSM_RECV );
@@ -106,71 +105,71 @@ always_comb begin : p_n_fsm_state
 end
 
 // --------------------------------------------------------------------------- 
-// Internal register setting and re-setting
+// Настройка и сброс внутренних регистров
 // ---------------------------------------------------------------------------
 
-// Handle updates to the recieved data register
+// Работа сдвигового регистра, принимающего поступающие данные
 always_ff @(posedge clk) begin : p_recieved_data
-    if(!resetn) begin
+    if (!resetn) begin
         recieved_data <= {PAYLOAD_BITS{1'b0}};
-    end else if(fsm_state == FSM_IDLE             ) begin
+    end else if (fsm_state == FSM_IDLE             ) begin
         recieved_data <= {PAYLOAD_BITS{1'b0}};
-    end else if(fsm_state == FSM_RECV && next_bit ) begin
+    end else if (fsm_state == FSM_RECV && next_bit ) begin
         recieved_data[PAYLOAD_BITS-1] <= bit_sample;
-        recieved_data <= recieved_data >> 1;
+        for (int i = PAYLOAD_BITS-2; i >= 0; i--) begin
+            recieved_data[i] <= recieved_data[i+1];
+        end
     end
 end
 
-// Increments the bit counter when recieving
+// Увеличение значения счётчика поступивших бит данных
 always_ff @(posedge clk) begin : p_bit_counter
-    if(!resetn) begin
+    if (!resetn) begin
         bit_counter <= {COUNT_REG_LEN{1'b0}};
-    end else if(fsm_state != FSM_RECV) begin
+    end else if (fsm_state != FSM_RECV) begin
         bit_counter <= {COUNT_REG_LEN{1'b0}};
-    end else if(fsm_state == FSM_RECV && next_bit) begin
+    end else if (fsm_state == FSM_RECV && next_bit) begin
         bit_counter <= bit_counter + 1'b1;
     end
 end
 
-// Sample the recieved bit when in the middle of a bit frame
+// Захват принятого бита, когда находимся по центру фрейма данного бита
 always_ff @(posedge clk) begin : p_bit_sample
-    if(!resetn) begin
+    if (!resetn) begin
         bit_sample <= 1'b0;
     end else if (cycle_counter == CYCLES_PER_BIT/2) begin
         bit_sample <= rxd_reg;
     end
 end
 
-// Increments the cycle counter when recieving
+// Работа счётчика циклов тактового сигнала в течение поступившего одного бита данных
 always_ff @(posedge clk) begin : p_cycle_counter
-    if(!resetn) begin
+    if (!resetn) begin
         cycle_counter <= {COUNT_REG_LEN{1'b0}};
-    end else if(next_bit) begin
+    end else if (next_bit) begin
         cycle_counter <= {COUNT_REG_LEN{1'b0}};
-    end else if(fsm_state == FSM_START || 
-                fsm_state == FSM_RECV  || 
-                fsm_state == FSM_STOP   ) begin
+    end else if (fsm_state == FSM_START || 
+                 fsm_state == FSM_RECV  || 
+                 fsm_state == FSM_STOP   ) begin
         cycle_counter <= cycle_counter + 1'b1;
     end
 end
 
-// Progresses the next FSM state.
+// Условие переключения в следующее состояние
 always_ff @(posedge clk) begin : p_fsm_state
-    if(!resetn) begin
+    if (!resetn) begin
         fsm_state <= FSM_IDLE;
     end else begin
         fsm_state <= n_fsm_state;
     end
 end
 
-// Responsible for updating the internal value of the rxd_reg
+// Синхронизация данных, принятых извне через два регистра
 always_ff @(posedge clk) begin : p_rxd_reg
-    if(!resetn) begin
-        rxd_reg   <= 1'b1;
-        rxd_reg_0 <= 1'b1;
-    end else if(uart_rx_en) begin
-        rxd_reg   <= rxd_reg_0;
-        rxd_reg_0 <= uart_rxd;
+    if (!resetn) begin
+        rxd_reg <= 2'b11;
+    end else if (uart_rx_en) begin
+        rxd_reg <= {rxd_reg[0], uart_rxd};
     end
 end
 
