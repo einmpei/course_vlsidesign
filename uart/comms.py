a0a268dad07fea7cc2edaa8faf8e32352721ff4c
adbarinov <115462546+adbarinov@users.noreply.github.com>
2023-12-28 21:16:33 +0300

    Добавил комментарии к открытию порта /dev/ttyUSB0

--- uart/comms.py
+++ uart/comms.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+#Переходник USB-RS232 при подключении в Linux создаёт виртуальный порт ttyUSB0
+#Для настройки доступа к порту /dev/ttyUSB0 необходимо добавить пользователя в группу dialout:
+#sudo usermod -aG dialout $USER
+#Может понадобится команда
+#sudo chmod 777 -R /dev/ttyUSB0
+#Открывать терминал можно только после подключения переходника USB-RS232, чтобы был виден порт
+
 import serial
 
-port = "/dev/ttyUSB0"
-baudrate = 9600
-message = b'Hello, World!'
+port = "/dev/ttyUSB0" #имя порта, в Windows это может быть COM1
+baudrate = 9600 #бод
+message = b'Hello, World!' #Передаваемое сообщение в бинарном формате
 
 print ("Opening %s" % port)
 ser = serial.Serial(port, baudrate = baudrate, parity = 'N', timeout = 2, bytesize=8, stopbits=1, rtscts=False, xonxoff=False)
